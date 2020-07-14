17e7870ab7cfa46dcc6fa0be3c309aa1aa8633ae
Asztalos Gergő <gergo0317@gmail.com>
2020-07-14 20:51:45 +0200

    implementing tree traversal method
    changing target to es6 for using simple ts methods

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import {traverse} from "./treeTraversal";
 
-export const treeTraversal = (list: any[]) => traverse(list);
+export const treeTraversal = (tree: any | any[], modifyHandler: (object: any) => any) => traverse(tree, modifyHandler);
--- src/treeTraversal.ts
+++ src/treeTraversal.ts
@@ -1,3 +1,25 @@
-export function traverse(list: any[]) {
-    return 'test';
+import {
+    keys as _keys,
+    isArray as _isArray,
+} from 'lodash';
+
+export function traverse(tree: any | any[], modifyHandler: (object: any) => any, childrenProp: string = 'children') {
+    if (_isArray(tree)) {
+        tree.forEach((object) => {
+            const childArray = _keys(object).map(key => object[key]).find(_isArray);
+            const modifiedObject = modifyHandler(object);
+            // Bypass immutable assignment
+            _keys(object).forEach((key) => {
+                if (modifiedObject) {
+                    object[key] = modifiedObject[key];
+                } else {
+                    object[key] = null;
+                }
+            });
+            if (childArray && childArray.length) {
+                object[childrenProp] = traverse(childArray, modifyHandler, childrenProp);
+            }
+        });
+    }
+    return tree;
 }
