39ede2cfc540a5ec6d92296880770aa923431f27
Asztalos Gergő <gergo0317@gmail.com>
2020-07-14 21:58:19 +0200

    fixing chritical bug

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import {traverse} from "./tree-traversal";
 import {removeEmptyProperties} from "./object-utils";
 
 export const treeTraversal = (tree: any | any[], modifyHandler: (object: any) => any) => {
-    removeEmptyProperties(traverse(tree, modifyHandler));
+    return removeEmptyProperties(traverse(tree, modifyHandler));
 };
