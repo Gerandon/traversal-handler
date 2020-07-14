bde052722905a12fa59e23b247a489ad9943bc42
Asztalos Gergő <gergo0317@gmail.com>
2020-07-14 22:03:02 +0200

    Minor fix, formatting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {traverse} from "./tree-traversal";
 import {removeEmptyProperties} from "./object-utils";
 
-export const treeTraversal = (tree: any | any[], modifyHandler: (object: any) => any) => {
-    return removeEmptyProperties(traverse(tree, modifyHandler));
-};
+export const treeTraversal = (
+    tree: any | any[],
+    modifyHandler: (object: any) => any,
+    childrenProperty?: string
+) => removeEmptyProperties(traverse(tree, modifyHandler, childrenProperty));
